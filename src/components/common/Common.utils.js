d02cbe891464e77e14fe3ed839bcac92ed0198c7
shubamWanclouds <sdadhwal@wanclouds.net>
2022-04-17 20:59:28 +0530

    round 2 digits updated to 3 digits

--- src/components/common/Common.utils.js
+++ src/components/common/Common.utils.js
@@ -1,6 +1,6 @@
 export const getTradingPairName = (tradingPair) => `${tradingPair?.slice(1,4)} / ${tradingPair?.slice(4)}`;
 
-export const roundTo2Digits = (price) => Number(price).toFixed(2);
+export const roundTo3Digits = (price) => Number(price).toFixed(3);
 
 export const addProfitLossSign = (percent) => percent > 0 ? `+${percent}` : percent;
 
