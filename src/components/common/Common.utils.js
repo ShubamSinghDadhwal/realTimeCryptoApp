456fa2926d01304d6bbbb9a061d217c9df63ef12
shubamWanclouds <sdadhwal@wanclouds.net>
2022-04-17 14:36:07 +0530

    common utils file added

---
+++ src/components/common/Common.utils.js
@@ -0,0 +1,7 @@
+export const getTradingPairName = (tradingPair) => `${tradingPair.slice(1,4)} / ${tradingPair.slice(4)}`;
+
+export const roundTo2Digits = (price) => Number(price).toFixed(2);
+
+export const addProfitLossSign = (percent) => percent > 0 ? `+${percent}` : percent;
+
+export const getProfitLossClass = (percent) => percent > 0 ? 'profit' : 'loss';
