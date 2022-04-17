b5ca22780cbc63a5a9c15954dd65caf560d2fc23
shubamWanclouds <sdadhwal@wanclouds.net>
2022-04-17 21:03:47 +0530

    tradepair highlight selection added

--- src/components/sidebar/tradingPairList/TradingPairList.view.js
+++ src/components/sidebar/tradingPairList/TradingPairList.view.js
@@ -3,27 +3,34 @@ import {
   addProfitLossSign,
   getProfitLossClass,
   getTradingPairName,
-  roundTo2Digits,
+  roundTo3Digits,
 } from '../../common/Common.utils';
 import './tradingPairList.css';
 
 const TradingPairList = (props) => {
-  const getData = (data) => {
+
+  const getData = (data, event) => {
+    document.querySelectorAll('.card').forEach(card =>
+      card.classList.remove('highlighted')
+    );
+    event.target.parentNode.classList.add("highlighted")
     setQueryParam(data[0]);
   }
+
   const { cryptoJson, setQueryParam } = props;
+  
   return (
     <div>
       {cryptoJson?.map(data => 
-        <div className='card' key={data[0]} onClick={() => getData(data)}>
+        <div className='card' key={data[0]} onClick={(event) => getData(data, event)}>
           <div className='card-row'>
             <p className='card-item trading-pair-symbol'>{getTradingPairName(data[0])}</p>
-            <p className='card-item current-value'>$ {roundTo2Digits(data[7])}</p>
+            <p className='card-item current-value'>$ {roundTo3Digits(data[7])}</p>
           </div>
           <div className='card-row'>
-            <p className='card-item volume'>Vol: {roundTo2Digits(data[8])}</p>
+            <p className='card-item volume'>Vol: {roundTo3Digits(data[8])}</p>
             <p className={`card-item ${getProfitLossClass(data[6])}`}>
-              {addProfitLossSign(roundTo2Digits(data[6]))}%
+              {addProfitLossSign(roundTo3Digits(data[6]))}%
             </p>
           </div>
         </div>
